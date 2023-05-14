3b291089bea1d354d50664f3008657df885f943a
malit mohamut <malitmohamud@gmail.com>
2023-05-13 23:33:55 -0700

    tokenize.c change

--- tokenize.c
+++ tokenize.c
@@ -1,37 +1,58 @@
-#include "main.h"
-/**
- * tokenize - Tokenize a string into an array of tokens
- * @line: The string to tokenize
- *
- * Return: A pointer to an array of token
- */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+#define DELIMITER " \t\r\n\a"
+
 char **tokenize(char *line)
 {
-	char **tokens = NULL;
-	char *token = NULL;
-	int index = 0;
+    char **tokens = NULL;
+    char *token = NULL;
+    int index = 0;
+    size_t size = 0;
 
-	tokens = malloc(sizeof(char *) * BUFFER_SIZE);
-	if (!tokens)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
+    if (line == NULL)
+        return NULL;
 
-	token = strtok(line, DELIMETER);
-	while (token != NULL)
-	{
-		tokens[index] = malloc(strlen(token) + 1);
-		if (!tokens[index])
-		{
-			perror("mallcc");
-			exit(EXIT_FAILURE);
-		}
-		strcpy(tokens[index], token);
-		index++;
-		token = strtok(NULL, DELIMETER);
-	}
+    tokens = malloc(BUFFER_SIZE * sizeof(char *));
+    if (tokens == NULL)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
-	tokens[index] = NULL;
-	return tokens;
+    token = strtok(line, DELIMITER);
+    while (token != NULL)
+    {
+        if (index >= BUFFER_SIZE)
+        {
+            fprintf(stderr, "tokenize: too many tokens\n");
+            free(tokens);
+            return NULL;
+        }
+
+        size = strlen(token) + 1;
+        tokens[index] = malloc(size * sizeof(char));
+        if (tokens[index] == NULL)
+        {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+
+        strcpy(tokens[index], token);
+        index++;
+
+        token = strtok(NULL, DELIMITER);
+    }
+
+    tokens[index] = NULL;
+
+    if (index == 0)
+    {
+        free(tokens);
+        tokens = NULL;
+    }
+
+    return tokens;
 }
